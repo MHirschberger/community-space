9f8e7853b333bc076cd1d5cf4df55b67c131525e
Michael Hirschberger <mlhirschberger@gmail.com>
2018-09-10 20:47:06 -0400

    Updated index.js and installed dependcies

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+    <Provider store={store} >
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
