a95e6042e5d0900160eedb7ae4995655cb0eddee
Michael Hirschberger <mlhirschberger@gmail.com>
2018-09-12 13:35:18 -0400

    Updated comment page

--- client/src/components/Comments/Comment.js
+++ client/src/components/Comments/Comment.js
@@ -1,2 +1,20 @@
+import React, { Component } from 'react';
+
+export default class Comment extends Component {
+
+  render() {
+    const { comment } = this.props
+
+    return (
+      <div>
+        <li>
+          {comment.text}
+        </li>
+        <button onClick={() => this.props.deleteComment(this.props.comment.id)}> X </button>
+      </div>
+    );
+  }
+
+};
 
 
--- client/src/containers/CommentsContainer.js
+++ client/src/containers/CommentsContainer.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react'
 import Comments from '../components/Comments/Comments';
+import CommentInput from '../components/Comments/CommentInput';
 import { connect } from 'react-redux';
 
 class CommentsContainer extends Component {
+
+    filterComments = () => (
+        this.props.comments.filter(comment => comment.discussionId === this.props.discussion.id)
+    )
 
   render() {
     return (
       <div>
         <CommentInput addComment={this.props.addComment} discussionId={this.props.discussion.id} />
-        <Comments comments={this.props.comments} deleteComment={this.props.deleteComment} />
+        <Comments comments={this.filterComments()} deleteComment={this.props.deleteComment} />
       </div>
     )
   }
