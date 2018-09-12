ce86f276bd1a6f1f4ad265cf9f74e67038db8a48
Michael Hirschberger <mlhirschberger@gmail.com>
2018-09-12 17:10:38 -0400

    Updated comments controller and added seed data

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,57 @@
 class CommentsController < ApplicationController
+
+    # skip_before_action :verify_authenticity_token
+    # before_action :require_login
+
+    def index
+        @discussion = Discussion.find(params[:discussion_id])
+        @comments = @discussion.comments
+        respond_to do |format|
+            format.json { render json: @comments }
+        end
+        @comment = Comment.new(discussion_id: params[:discussion_id])
+    end
+
+    def new
+        @comment = Comment.new(discussion_id: params[:discussion_id])
+    end
+
+    def create
+        @comment = Post.new(comment_params)
+
+        if @comment.save
+            @discussion = Discussion.find(@comment.discussion_id)
+            respond_to do |format|
+                format.json { render json: @discussion }
+            end
+        end
+        # else
+        #     @course = @post.course
+        #     @posts = @course.posts.order(id: :desc)
+        #     respond_to do |format|
+        #         format.html { render 'index' }
+        #         format.json { render json: @post.errors, status: :unprocessable_entity }
+        #     end
+
+        # end
+    end
+
+    def show
+        @comment = Comment.find_by(id: params[:id])
+        respond_to do |format|
+            format.json { render json: @comment, include: ['discussion'] }
+        end
+    end
+
+    def destroy
+        @comment = Comment.find_by(id: params[:id])
+        @comment.destroy
+    end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:text, :discussion_id)
+    end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Discussion.create([{text: 'Did you see that Jets vs Lions game this week?'}, {text: 'Fall activity ideas...'}, {text: "New Year's Resolution Ideas - 2019"}])
+
+Comment.create([{text: 'Yeah, it was insane!', discussion_id: 1}, {text: 'Matthew Stafford threw 4 interceptions!', discussion_id: 1}, {text: 'As a Jets fan I am not used to this!', discussion_id: 1},
+{text: 'Pumpkin picking?', discussion_id: 2}, {text: 'Skydiving?', discussion_id: 2}, {text: 'Bike ride through Central Park', discussion_id: 2},
+{text: 'Cutting out soda from my diet', discussion_id: 3}, {text: 'Exercising for one hour 5 days per week', discussion_id: 3}, {text: 'Complete reading 1 book every two weeks.', discussion_id: 3}])
