bc89e72e2fd87ad73c07916fd228335d0cbf4b8c
Michael Hirschberger <mlhirschberger@gmail.com>
2018-09-12 12:33:11 -0400

    Updated CommentsContainer

--- client/src/containers/CommentsContainer.js
+++ client/src/containers/CommentsContainer.js
@@ -7,18 +7,20 @@ class CommentsContainer extends Component {
   render() {
     return (
       <div>
-        <Comments comments={this.props.comments} deleteComment={this.props.deleteComment}/>
+        <CommentInput addComment={this.props.addComment} />
+        <Comments comments={this.props.comments} deleteComment={this.props.deleteComment} />
       </div>
     )
   }
 }
 
 const mapStateToProps = state => ({
-    discussions: state.discussions.discussions
+    comments: state.comments.comments
 })
 
 const mapDispatchToProps = dispatch => ({
-    deleteComment: id => dispatch({type: 'DELETE_DISCUSSION', id})
+    addComment: text => dispatch({type: 'ADD_COMMENT', text}),
+    deleteComment: id => dispatch({type: 'DELETE_COMMENT', id})
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(CommentsContainer);
