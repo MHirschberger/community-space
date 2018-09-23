cd9d1caa5cf1692c70390325dceaf2887af036fe
Michael Hirschberger <mlhirschberger@gmail.com>
2018-09-23 14:06:26 -0400

    Added users table to database and created User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_12_194509) do
+ActiveRecord::Schema.define(version: 2018_09_23_180116) do
 
   create_table "comments", force: :cascade do |t|
     t.string "text"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 2018_09_12_194509) do
 
   create_table "discussions", force: :cascade do |t|
     t.string "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
