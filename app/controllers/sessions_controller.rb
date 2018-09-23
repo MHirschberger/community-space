e12ff234951699753080a58d0cf416647e408e60
Michael Hirschberger <mlhirschberger@gmail.com>
2018-09-23 15:45:01 -0400

    Added Sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,33 @@
+class SessionsController < ApplicationController
+    def new
+        # if logged_in?
+        #   redirect_to user_path(current_user)
+        # end
+    end
+
+    def create
+        @user = User.find_by(name: params[:name])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            render json: @user
+        else
+            render json: @user.errors, status: :unprocessable_entity
+        end
+        
+        # session[:user_id] = @user.id
+        # render 'users/show'
+    end
+
+    end
+
+    def destroy
+    session.delete :user_id
+    redirect_to root_url
+    end
+
+    private
+
+    def auth
+    request.env['omniauth.auth']
+    end
+end
