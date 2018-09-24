1e399ffdf2e0e2a8b9ef5bf99a6fae7443747a5f
Michael Hirschberger <mlhirschberger@gmail.com>
2018-09-23 20:55:41 -0400

    Updated database to PostgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_09_23_180116) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string "text"
     t.integer "discussion_id"
